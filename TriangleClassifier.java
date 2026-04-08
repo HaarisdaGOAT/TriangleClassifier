a880e49982e5e9c7cda5ded7e13caa5d74abb575
HaarisdaGOAT <haariss3@educbe.ca>
2026-04-08 13:47:46 -0600

    Solved TriangleClassifier

--- TriangleClassifier.java
+++ TriangleClassifier.java
@@ -1,19 +1,4 @@
 public class TriangleClassifier {
-    public static String getClassification(int angleA, int angleB, int angleC) {
-        String sideType = classifyBySide(angleA, angleB, angleC);
-        String angleType = classifyByAngle(AngleA, angleB, angleC);
-
-        if (!isValidTriangle(angleA, angleB, angleC)) {
-            return "INVALID";
-        }
-
-        if (sideType.equals("equilateral")) {
-            return "equilateral";
-        }
-
-        return angleType+ " " + sideType;
-
-    }
 
     public static boolean isValidTriangle(int angleA, int angleB, int angleC) {
         return (angleA > 0 && angleB > 0 && angleC > 0) && (angleA + angleB + angleC == 0);
@@ -27,8 +12,32 @@ public class TriangleClassifier {
         } else {
             return "scalene";
         }
+    }
 
-    
+    public static String classifyByAngle(int angleA, int angleB, int angleC) {
+        if (angleA == 90 || angleB == 90 || angleC == 90) {
+            return "right";
+        } else if (angleA > 90 || angleB > 90 || angleC > 90) {
+            return "obtuse";
+        } else {
+            return "acute";
+        }
+    }
+
+    public static String getClassification(int angleA, int angleB, int angleC) {
+        String sideType = classifyBySide(angleA, angleB, angleC);
+        String angleType = classifyByAngle(angleA, angleB, angleC);
+
+        if (!isValidTriangle(angleA, angleB, angleC)) {
+            return "INVALID";
+        }
+
+        if (sideType.equals("equilateral")) {
+            return "equilateral";
+        }
+
+        return angleType+ " " + sideType;
 
     }
+
 }
